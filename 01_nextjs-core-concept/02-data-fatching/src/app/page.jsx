8697a0c442a6f9a968901fedfadebb474b3bbeec
parvez-prottoy <parvezahmedprottoy.web@gmail.com>
2026-09-17 10:52:43 +0600

    feat: initialize data  fetching Next.js project with Tailwind CSS and DaisyUI

---
+++ 01_nextjs-core-concept/02-data-fatching/src/app/page.jsx
@@ -0,0 +1,17 @@
+export default function HomePage() {
+  return (
+    <div className="hero bg-base-100 py-20">
+      <div className="hero-content text-center">
+        <div className="max-w-md">
+          <h1 className="text-5xl font-bold">Welcome to Foods</h1>
+          <p className="py-6">
+            Discover a world of delicious flavors and culinary delights. Explore
+            our curated selection of foods and reviews to satisfy your taste
+            buds.
+          </p>
+          <button className="btn btn-soft">Get Started</button>
+        </div>
+      </div>
+    </div>
+  );
+}
